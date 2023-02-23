245170e83ea0535488b122fedda831c03a428869
SeungHun <hunhunseunghun@gmail.com>
2023-02-23 21:38:29 +0900

    .

--- src/pages/Options/index.jsx
+++ src/pages/Options/index.jsx
@@ -5,5 +5,6 @@ import Options from './Options';
 import './index.css';
 
 const container = document.getElementById('app-container');
+const container1 = document.getElementById('app-container');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(<Options title={'Settings'} />);
